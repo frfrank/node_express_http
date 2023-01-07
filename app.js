94356bda92af0d57330b3a73ffec876d126ab424
Francisco Picado <franciscopicado19@gmail.com>
2023-01-07 14:33:58 +0100

    configuration Handebars

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 const port = 8080;
 
+app.set('view engine', 'hbs');
 //Servir contenido estatico
 app.use(express.static('public'));
 
 app.get('/',  (req, res) => {
-    res.send('Hello World');
+   res.render('home');
 });
 
 app.get('/home/generic', (req, res) =>  {
